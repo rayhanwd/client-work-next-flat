0693c8d7ffc44245c4cab23fb52edb2b560bbb6e
AMFahim <amfahim2019@gmail.com>
2023-12-04 00:30:30 +0600

    details page

--- src/app/(ads)/details/page.tsx
+++ src/app/(ads)/details/page.tsx
@@ -1,25 +1,49 @@
+import Flag from "@/app/icons/Flag";
+import LocationFlag from "@/app/icons/LocationFlag";
+import LoveIcon from "@/app/icons/LoveIcon";
+import UrlIcon from "@/app/icons/UrlIcon";
+import XIcon from "@/app/icons/XIcon";
 import Image from "next/image";
 
 const details = () => {
     return (
         <>
-            <div className=" grid grid-cols-2">
-                <div>
-                    <h1>Grid 1</h1>
+            <div className=" grid grid-cols-2 justify-between gap-2 mt-[30px]">
+                <div className="ml-[53px]">
+                    <h1 className=" font-inter text-2xl font-medium">Flat</h1>
+                    <div className="flex justify-end gap-3">
+                        <div className="w-[31.36px] flex items-center justify-center h-[32.30px] bg-slate-100 rounded-full border border-slate-200">
+                            <Flag />
+                        </div>
+                        <div className=" w-[31.36px] h-[32.30px] flex items-center justify-center bg-slate-100 rounded-full border border-slate-200">
+                            <UrlIcon />
+                        </div>
+                        <div className="w-[31.36px] h-[32.30px] flex items-center justify-center bg-slate-100 rounded-full border border-slate-200">
+                            <LocationFlag />
+                        </div>
+                        <div className="w-[31.36px] h-[32.30px] flex items-center justify-center bg-slate-100 rounded-full border border-slate-200">
+                            <LoveIcon />
+                        </div>
+                        <div className="w-[31.36px] h-[32.30px] ml-[29px] flex items-center justify-center bg-slate-100 rounded-full border border-slate-200">
+                            <XIcon />
+                        </div>
+                    </div>
                 </div>
+
+                {/* parent 2  */}
                 <div>
-                    <Image 
-                    src="/map.png"
-                    alt="map"
-                    width={732}
-                    height={929}
+                    <Image
+                        src="/map.png"
+                        alt="map"
+                        width={732}
+                        height={929}
                     />
-                    <Image 
-                    src="/graph.png"
-                    alt="Graph Chart"
-                    width={754}
-                    height={238}
-                    className="mt-[28px]"
+                    <Image
+                        src="/graph.png"
+                        alt="Graph Chart"
+                        width={754}
+                        height={238}
+                        className="mt-[28px]"
                     />
                 </div>
 
---
+++ src/app/icons/XIcon.tsx
@@ -0,0 +1,21 @@
+
+function XIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="19"
+      height="19"
+      fill="none"
+      viewBox="0 0 19 19"
+    >
+      <path
+        fill="#0F172A"
+        fillRule="evenodd"
+        d="M4.73 6.17a1 1 0 011.415-1.414l3.793 3.793 3.793-3.793a1 1 0 111.414 1.414l-3.793 3.793 3.793 3.793a1 1 0 01-1.414 1.414l-3.793-3.793-3.793 3.793a1 1 0 11-1.414-1.414l3.793-3.793L4.73 6.17z"
+        clipRule="evenodd"
+      ></path>
+    </svg>
+  );
+}
+
+export default XIcon;
