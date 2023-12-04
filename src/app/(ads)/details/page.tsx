1998805979396216b15beb571339dd33190a6b0c
AMFahim <amfahim2019@gmail.com>
2023-12-04 21:24:34 +0600

    page updated

--- src/app/(ads)/details/page.tsx
+++ src/app/(ads)/details/page.tsx
@@ -1,9 +1,22 @@
+"use-client"
 import Flag from "@/app/icons/Flag";
 import LocationFlag from "@/app/icons/LocationFlag";
 import LoveIcon from "@/app/icons/LoveIcon";
 import UrlIcon from "@/app/icons/UrlIcon";
 import XIcon from "@/app/icons/XIcon";
 import Image from "next/image";
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+
+// import required modules
+import { Pagination, Navigation } from 'swiper/modules';
 
 const details = () => {
     return (
@@ -28,7 +41,30 @@ const details = () => {
                             <XIcon />
                         </div>
                     </div>
+
+                    <div>
+                        <Swiper
+                            pagination={{
+                                type: 'fraction',
+                            }}
+                            navigation={true}
+                            modules={[Pagination, Navigation]}
+                            className="mySwiper"
+                        >
+                            <SwiperSlide>Slide 1</SwiperSlide>
+                            <SwiperSlide>Slide 2</SwiperSlide>
+                            <SwiperSlide>Slide 3</SwiperSlide>
+                            <SwiperSlide>Slide 4</SwiperSlide>
+                            <SwiperSlide>Slide 5</SwiperSlide>
+                            <SwiperSlide>Slide 6</SwiperSlide>
+                            <SwiperSlide>Slide 7</SwiperSlide>
+                            <SwiperSlide>Slide 8</SwiperSlide>
+                            <SwiperSlide>Slide 9</SwiperSlide>
+                        </Swiper>
+                    </div>
                 </div>
+
+
 
                 {/* parent 2  */}
                 <div>
