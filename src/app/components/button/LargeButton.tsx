570f9b72bcc67b15de8edf2dcc3f55a9b4c79b15
rayhanwd <rayhanbd4400@gmail.com>
2023-12-10 20:07:38 +0600

    updated

--- src/app/components/button/LargeButton.tsx
+++ src/app/components/button/LargeButton.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 
-const LargeButton = ({label, classes = ''}) => {
-    return (
-        <div>
-            <button className={`w-full bg-blue-700 rounded-lg px-5 py-2.5 text-white text-lg font-semibold font-outfit capitalize leading-7 tracking-tight mt-6 ${classes}`}>
-                {label}
-            </button>
-        </div>
-    );
+const LargeButton = ({ label, classes = "" }) => {
+  return (
+    <div>
+      <button
+        className={`w-full bg-blue-700 rounded-lg px-5 py-2.5 text-white text-lg font-semibold font-outfit capitalize leading-7 tracking-tight mt-6 ${classes}`}
+      >
+        {label}
+      </button>
+    </div>
+  );
 };
 
 export default LargeButton;
